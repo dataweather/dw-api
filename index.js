2b1347d220aadf492144211dda9b28bdad2c78c5
Lucas <lucas@agtlucas.com>
2015-03-21 21:24:56 -0300

    Update index.js

--- index.js
+++ index.js
@@ -26,6 +26,19 @@ server.route({
 
 server.register({
   register: Good,
-})
+  options: {
+    reporters: [{
+      reporter: require('good-console'),
+      args: [{ log: '*', response: '*' }]
+    }]
+  }
+}, function (err) {
+  if (err) {
+    throw err;
+  }
 
-server.start();
+  server.start(function () {
+    server.log('info', 'Server running at: ' + server.info.uri);
+  });
+
+});
